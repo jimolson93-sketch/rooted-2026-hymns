4faf3a316ab680a936e8055e427c1d1376a760a7
jimolson93-sketch <jim.olson93@gmail.com>
2026-08-15 12:16:21 -0500

    Cache word search release

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const CACHE_PREFIX='rooted-hymns-';
-const CACHE_VERSION='2026.08.11.13';
+const CACHE_VERSION='2026.08.11.14';
 const APP_FILES=[
   './',
   './index.html',
   './manifest.webmanifest',
   './css/styles.css?v=20260811-5',
   './data/hymns.js?v=20260811-5',
-  './js/app.js?v=20260815-2',
+  './js/app.js?v=20260815-3',
   './js/reading-enhancements.js?v=20260810-3',
   './js/update.js?v=20260811-3',
   './version.json'
